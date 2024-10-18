0592481b0c1279e16817954039dc85b389b0f891
flynx <admin@flynx.co>
2024-10-18 09:33:38 -0500

    UPDATE: actualizando la url base del dominio personalizado

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://adminflynx.github.io/",
+  base: "https://www.flynx.co",
   plugins: [
     // comlink(),
     vue(),
